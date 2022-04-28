b004dccb97b7eddf5d9c5fce4e058485f95c2edf
Mohsen-Shah <Mohsen-Shah@DESKTOP-90CCE10>
2022-04-28 17:49:46 +0430

    Product Page Implemented

--- Make-Up/01-MakeUpQuery/Contracts/Product/IProductQuery.cs
+++ Make-Up/01-MakeUpQuery/Contracts/Product/IProductQuery.cs
@@ -4,6 +4,7 @@ namespace _01_MakeUpQuery.Contracts.Product
 {
     public interface IProductQuery
     {
+        ProductQueryModel GetDetails(string slug);
         List<ProductQueryModel> GetLatestArrivals();
         List<ProductQueryModel> Search(string value);
     }
--- Make-Up/01-MakeUpQuery/Query/ProductQuery.cs
+++ Make-Up/01-MakeUpQuery/Query/ProductQuery.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using _0_Framework.Application;
 using _01_MakeUpQuery.Contracts.Product;
-using _01_MakeUpQuery.Contracts.ProductCategory;
 using DiscountManagement.Infrastructure.EFCore;
 using InventoryManagement.Infrastructure.EFCore;
 using Microsoft.EntityFrameworkCore;
-using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductPictureAgg;
 using ShopManagement.Infrastructure.EFCore;
 
 namespace _01_MakeUpQuery.Query
@@ -23,6 +22,77 @@ namespace _01_MakeUpQuery.Query
             _context = context;
             _inventoryContext = inventoryContext;
             _discountContext = discountContext;
+        }
+
+        public ProductQueryModel GetDetails(string slug)
+        {
+            var inventory = _inventoryContext.Inventory.Select(x => new {x.ProductId, x.UnitPrice, x.InStock}).ToList();
+
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(customerDiscount =>
+                    customerDiscount.StartDate < DateTime.Now && customerDiscount.EndDate > DateTime.Now)
+                .Select(customerDiscount => new
+                    {customerDiscount.ProductId, customerDiscount.DiscountRate, customerDiscount.EndDate}).ToList();
+
+            var product = _context.Products
+                .Include(product => product.Category)
+                .Include(product => product.ProductPicture)
+                .Select(product =>
+                    new ProductQueryModel
+                    {
+                        Id = product.Id,
+                        Category = product.Category.Name,
+                        Name = product.Name,
+                        Picture = product.Picture,
+                        PictureAlt = product.PictureAlt,
+                        PictureTitle = product.PictureTitle,
+                        Slug = product.Slug,
+                        CategorySlug = product.Category.Slug,
+                        Description = product.Description,
+                        Code = product.Code,
+                        Keywords = product.Keywords,
+                        MetaDescription = product.MetaDescription,
+                        ShortDescription = product.ShortDescription,
+                        Pictures = MapProductPictures(product.ProductPicture),
+                    }).AsNoTracking().FirstOrDefault(x => x.Slug == slug);
+
+
+            if (product == null)
+                return new ProductQueryModel();
+
+            var productInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
+
+
+            if (productInventory != null)
+            {
+                product.IsInStock = productInventory.InStock;
+                var price = productInventory.UnitPrice;
+                product.Price = price.ToMoney();
+                var discount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+                if (discount != null)
+                {
+                    int discountRate = discount.DiscountRate;
+                    product.DiscountRate = discountRate;
+                    product.HasDiscount = discountRate > 0;
+                    product.DiscountExpireDate = discount.EndDate.ToDiscountFormat();
+                    var discountAmount = Math.Round((price * discountRate) / 100);
+                    product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                }
+            }
+
+            return product;
+        }
+
+        private static List<ProductPictureQueryModel> MapProductPictures(List<ProductPicture> pictures)
+        {
+            return pictures.Select(x => new ProductPictureQueryModel
+            {
+                IsRemoved = x.IsRemoved,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                ProductId = x.ProductId 
+            }).Where(x => !x.IsRemoved).ToList();
         }
 
         public List<ProductQueryModel> GetLatestArrivals()
